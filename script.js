8b6a5b1c8572e8a6b6a14822788212688183efc8
Érick <eierickdasilva@gmail.com>
2024-02-17 06:39:05 -0300

    Feature: adding the full app

---
+++ script.js
@@ -0,0 +1,20 @@
+const textarea = document.querySelector(".textarea");
+const saveBtn = document.querySelector("#save-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+saveBtn.addEventListener("click", () => {
+    save()
+});
+
+clearBtn.addEventListener("click", () => {
+    textarea.value = "";
+});
+
+function save() {
+    const text = textarea.value;
+    const file = new Blob([text], {type: "text/plan"});
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(file);
+    link.download = "text.txt";
+    link.click();
+}
