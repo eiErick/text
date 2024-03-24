a01f70940dfaac5e12f82afcf13edd1b586701cf
Érick <eierickdasilva@gmail.com>
2024-03-24 11:08:04 -0300

    Feature: adding electron js to run on desktop app

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 310,
+    minHeight: 250,
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.setMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
